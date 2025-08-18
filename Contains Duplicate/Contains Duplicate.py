602ff9fa7166a6b44fe29b3ccd19256fac803937
LogicalDrift <98732234+TechieAnubhav@users.noreply.github.com>
2025-08-18 22:30:26 +0530

    Create Contains Duplicate.py

---
+++ Contains Duplicate/Contains Duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=""
+            else:
+                return True
+                break
+        else:
+            return False
