1eae0e97beda6ee8517c67a85ca9a892f11be9a0
LogicalDrift <98732234+TechieAnubhav@users.noreply.github.com>
2025-09-01 19:19:17 +0530

    Update Pascal's Triangle.py

--- Pascal's Triangle/Pascal's Triangle.py
+++ Pascal's Triangle/Pascal's Triangle.py
@@ -1,10 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        l=[]
-        for i in range(numRows):
-            s=str(11**i)
-            m=[]
-            for j in s:
-                m.append(int(j))
-            l.append(m)
-        return l
+        result= []
+        if numRows==0:
+            return result
+        
+        first_row=[1]
+        result.append(first_row)
+        for i in range(1,numRows):
+            prev_row=result[i-1]
+            current_row=[1]
+            for j in range(1,i):
+                current_row.append(prev_row[j-1] + prev_row[j])
+            current_row.append(1)
+            result.append(current_row)
+        return result
