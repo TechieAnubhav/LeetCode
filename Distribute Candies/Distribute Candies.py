ed2c3f1bbd22a92de90eaaebfdf2b76ae311db8d
LogicalDrift <98732234+TechieAnubhav@users.noreply.github.com>
2025-09-26 23:29:28 +0530

    Create Distribute Candies.py

---
+++ Distribute Candies/Distribute Candies.py
@@ -0,0 +1,8 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n=len(candyType)//2
+        s=set(candyType)
+        if n>=len(s):
+            return len(s)
+        else:
+            return n
