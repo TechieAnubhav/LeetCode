229b3b1fd18dc06dcfbce85114b16e98352ba20e
Anubhav Singh <98732234+LogicalDrift@users.noreply.github.com>
2026-05-22 23:47:10 +0530

    Create Build an Array With Stack Operations.cpp

---
+++ Build an Array With Stack Operations/Build an Array With Stack Operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        for (int i = 1, j = 0; i <= target.back(); i++) {
+            ans.push_back("Push");
+            if(target[j] != i){
+                ans.push_back("Pop");
+            } 
+            else {
+                j++;
+            }
+        }
+        return ans; 
+    }
+};
